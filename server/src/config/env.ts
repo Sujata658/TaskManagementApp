fa86675dde73f9ef08c90c66c3289435a466c61b
Sujata <076bct085.sujata@pcampus.edu.np>
2024-05-20 17:02:13 +0545

    server boilerplate

---
+++ server/src/config/env.ts
@@ -0,0 +1,11 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+  port: process.env.PORT || 5000,
+  cors: process.env.CORS,
+  mongoDbConnectionUrl: process.env.MONGO_URL,
+  accessKeySecret: process.env.ACCESS_KEY || 'access',
+  refreshKeySecret: process.env.REFRESH_KEY || 'refresh',
+};
---
+++ server/src/routes/v1/index.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+
+import Health from './Health';
+
+const router = Router();
+router.use('/health', Health);
+
+/**
+ * Import and add your routes here
+ * Eg:
+ *   router.use('/[route-name]', [Route]);
+ */
+
+export default router;
